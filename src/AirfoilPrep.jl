1a31961318ca9c2e6091e5f419ff4a5c584bfad3
moore54 <moorekevin711@gmail.com>
2018-01-26 16:53:47 -0700

    Will need to figure out airfoilprep.py dependency...

--- src/AirfoilPrep.jl
+++ src/AirfoilPrep.jl
@@ -1,5 +1,298 @@
 module AirfoilPrep
 
-# package code goes here
+
+
+# ------------ GENERIC MODULES -------------------------------------------------
+using Dierckx
+using Roots
+using PyCall
+using PyPlot
+using QuadGK
+
+
+# ------------ GLOBAL VARIABLES ------------------------------------------------
+modulepath,_ = splitdir(@__FILE__)
+data_path = modulepath*"../data/"
+
+# ------------ OTHER MODULES ------------------------------------------------
+push!(LOAD_PATH, joinpath(modulepath,"jlxlight/julia/"))
+using Xfoil
+
+# Wrap airfoilprep.py
+@pyimport imp
+(file, filename, data) = imp.find_module("airfoilprep", [modulepath])
+prepy = imp.load_module("airfoilprep", file, filename, data)
+
+include("airfoilprep_misc.jl")
+
+
+
+################################################################################
+# airfoilprep.py WRAPPER CLASS
+################################################################################
+"""
+  `Polar(Re, alpha, cl, cd, cm, x, y)`
+
+Defines section lift, drag, and pitching moment coefficients as a
+function of angle of attack at a particular Reynolds number. This object acts
+as an interface for the Python object `Polar` from `airfoilprep.py`, with
+the following properties:
+
+  * `self.PyPolar[:Re]`     : (float) Reynolds number.
+  * `self.PyPolar[:alpha]`  : (array) Angles of attacks.
+  * `self.PyPolar[:cl]`     : (array) Lift coefficient at each AOA.
+  * `self.PyPolar[:cd]`     : (array) Drag coefficient at each AOA.
+  * `self.PyPolar[:cm]`     : (array) Moment coefficient at each AOA.
+
+  # Arguments
+  * Re::Int64               : Reynolds number of this polar
+  * alpha::Array{Float64,1} : Angles of attack (deg)
+  * cl::Array{Float64,1}    : Lift coefficient at each AOA.
+  * cd::Array{Float64,1}    : Drag coefficient at each AOA.
+  * cm::Array{Float64,1}    : Moment coefficient at each AOA.
+  * x::Array{Float64,1}     : x-coordinates of airfoil geometry
+  * y::Array{Float64,1}     : y-coordinates of airfoil geometry
+
+NOTE: `alpha` input and output is always in degrees.
+**NOTE 2: Airfoil points x,y must go from trailing edge around the top, then the
+bottom and end back at the trailing edge.**
+"""
+type Polar
+  # Initialization variables (USER INPUT)
+  init_Re::Int64                          # Reynolds number of this polar
+  init_alpha::Array{Float64,1}            # Angles of attack (deg)
+  init_cl::Array{Float64,1}               # Lift coefficient at each AOA.
+  init_cd::Array{Float64,1}               # Drag coefficient at each AOA.
+  init_cm::Array{Float64,1}               # Moment coefficient at each AOA.
+  x::Array{Float64,1}                     # x-coordinates of airfoil geometry
+  y::Array{Float64,1}                     # y-coordinates of airfoil geometry
+
+  # Internal variables
+  pyPolar::PyCall.PyObject
+
+  Polar(init_Re, init_alpha, init_cl, init_cd, init_cm, x, y,
+          pyPolar=prepy[:Polar](init_Re, init_alpha, init_cl, init_cd, init_cm)
+        ) = new(
+        init_Re, init_alpha, init_cl, init_cd, init_cm, x, y,
+          pyPolar)
+end
+
+"Returns (alpha, cl) points of this Polar"
+function get_cl(self::Polar)
+  return (self.pyPolar[:alpha], self.pyPolar[:cl])
+end
+"Returns (alpha, cd) points of this Polar"
+function get_cd(self::Polar)
+  return (self.pyPolar[:alpha], self.pyPolar[:cd])
+end
+"Returns (alpha, cm) points of this Polar"
+function get_cm(self::Polar)
+  return (self.pyPolar[:alpha], self.pyPolar[:cm])
+end
+"Returns (x, y) points of the airfoil geometry of this Polar"
+function get_geometry(self::Polar)
+  return (self.x, self.y)
+end
+"Returns a dummy polar object"
+function dummy_polar()
+  return Polar(-1, Float64[], Float64[], Float64[], Float64[],
+    Float64[], Float64[])
+end
+
+"""
+  `correction3D(self::Polar, r_over_R::Float64, chord_over_r::Float64,
+                        tsr::Float64; alpha_max_corr=30, alpha_linear_min=-5,
+                        alpha_linear_max=5)`
+
+Applies 3-D corrections for rotating sections from the 2-D data.
+
+  Parameters
+  ----------
+  r_over_R : float
+      local radial position / rotor radius
+  chord_over_r : float
+      local chord length / local radial location
+  tsr : float
+      tip-speed ratio
+  alpha_max_corr : float, optional (deg)
+      maximum angle of attack to apply full correction
+  alpha_linear_min : float, optional (deg)
+      angle of attack where linear portion of lift curve slope begins
+  alpha_linear_max : float, optional (deg)
+      angle of attack where linear portion of lift curve slope ends
+
+  Returns
+  -------
+  polar : Polar
+      A new Polar object corrected for 3-D effects
+
+  Notes
+  -----
+  The Du-Selig method :cite:`Du1998A-3-D-stall-del` is used to correct lift, and
+  the Eggers method :cite:`Eggers-Jr2003An-assessment-o` is used to correct drag.
+"""
+function correction3D(self::Polar, r_over_R::Float64, chord_over_r::Float64,
+                      tsr::Float64; alpha_max_corr=30, alpha_linear_min=-5,
+                      alpha_linear_max=5)
+  new_pyPolar = self.pyPolar[:correction3D](r_over_R, chord_over_r, tsr,
+                  alpha_linear_min=alpha_linear_min,
+                  alpha_linear_max=alpha_linear_max,
+                  alpha_max_corr=alpha_max_corr)
+  new_polar = _pyPolar2Polar(new_pyPolar, self.x, self.y)
+  return new_polar
+end
+
+"""
+  `extrapolate(self::Polar, cdmax::Float64; AR=nothing, cdmin=0.001,
+                    nalpha=15)`
+Extrapolates force coefficients up to +/- 180 degrees using Viterna's method
+:cite:`Viterna1982Theoretical-and`.
+
+  Parameters
+  ----------
+  cdmax : float
+      maximum drag coefficient
+  AR : float, optional
+      aspect ratio = (rotor radius / chord_75% radius)
+      if provided, cdmax is computed from AR
+  cdmin: float, optional
+      minimum drag coefficient.  used to prevent negative values that can
+      sometimes occur with this extrapolation method
+  nalpha: int, optional
+      number of points to add in each segment of Viterna method
+
+  Returns
+  -------
+  polar : Polar
+      a new Polar object
+
+  Notes
+  -----
+  If the current polar already supplies data beyond 90 degrees then
+  this method cannot be used in its current form and will just return itself.
+
+  If AR is provided, then the maximum drag coefficient is estimated as
+
+  cdmax = 1.11 + 0.018*AR
+
+"""
+function extrapolate(self::Polar, cdmax::Float64; AR=nothing, cdmin=0.001,
+                      nalpha=15)
+  new_pyPolar = self.pyPolar[:extrapolate](cdmax, AR=AR, cdmin=cdmin,
+                                            nalpha=nalpha)
+  new_polar = _pyPolar2Polar(new_pyPolar, self.x, self.y)
+  return new_polar
+end
+
+"Plots this Polar. Give it `geometry=(x,y,1)` for ploting the airfoil geometry,
+where x and y are the points of the airfoil (the third number gives the size
+of the plot)"
+function plot(self::Polar; geometry::Bool=true, label="", style=".-",
+                              cdpolar=true)
+
+  # Geometry
+  if geometry
+    x,y = get_geometry(self)
+    plot_airfoil(x, y; label=label, style="-", figfactor=1.0)
+  end
+
+  fig2 = figure("polar_curves", figsize=(7*3,5*1))
+
+  alpha, cl = get_cl(self)
+  _, cd = get_cd(self)
+  _, cm = get_cm(self)
+
+  # Lift
+  subplot(131)
+  title("Lift curve at Re=$(self.pyPolar[:Re])")
+  PyPlot.plot(alpha, cl, style, label=label)
+  xlabel(L"Angle of attack $\alpha (^\circ)$")
+  ylabel(L"C_l")
+  grid(true, color="0.8", linestyle="--")
+  if label!=""; legend(loc="best"); end;
+
+  subplot(132)
+  title("Drag polar at Re=$(self.pyPolar[:Re])")
+  PyPlot.plot( cdpolar ? cl : alpha, cd, style, label=label)
+  if cdpolar
+    xlabel(L"C_l")
+  else
+    xlabel(L"Angle of attack $\alpha (^\circ)$")
+  end
+  ylabel(L"C_d")
+  grid(true, color="0.8", linestyle="--")
+  if label!=""; legend(loc="best"); end;
+
+  subplot(133)
+  title("Moment curve at Re=$(self.pyPolar[:Re])")
+  PyPlot.plot(alpha, cm, style, label=label)
+  xlabel(L"Angle of attack $\alpha (^\circ)$")
+  ylabel(L"C_m")
+  grid(true, color="0.8", linestyle="--")
+  if label!=""; legend(loc="best"); end;
+end
+
+"Compares two polars. Returns [err1, err2, err3] with an error between 0 and 1
+of Cl, Cd, and Cm curves, respectively."
+function compare(polar1::Polar, polar2::Polar; verbose::Bool=false)
+  out = []
+  curves = [:cl, :cd, :cm]
+
+  # Determines the range of alphas to compare
+  min_alpha1 = minimum(polar1.pyPolar[:alpha])
+  min_alpha2 = minimum(polar2.pyPolar[:alpha])
+  max_alpha1 = maximum(polar1.pyPolar[:alpha])
+  max_alpha2 = maximum(polar2.pyPolar[:alpha])
+  min_alpha = max(min_alpha1, min_alpha2)
+  max_alpha = min(max_alpha1, max_alpha2)
+  if max_alpha < min_alpha # Case that ranges don't intercept
+    warn("Requested comparison of polar that don't coincide. Returning -1.")
+    return [-1,-1,-1]
+  end
+
+  if verbose; println("min_alpha:$min_alpha\tmax_alpha:$max_alpha"); end;
+
+  # Creates splines of each curve on each polar
+  splines = Dict()
+  for polar in [polar1, polar2] # each polar
+    this_splines = []
+    for curve in curves # each curve
+      this_spline = Dierckx.Spline1D(polar.pyPolar[:alpha], polar.pyPolar[curve])
+      push!(this_splines, this_spline)
+    end
+    splines[polar] = this_splines
+  end
+
+  for (i,curve) in enumerate(curves)
+    f1(x) = splines[polar1][i](x)
+    f2(x) = splines[polar2][i](x)
+    absf1(x) = abs(f1(x))
+    absf2(x) = abs(f2(x))
+    dif(x) = abs(f2(x) - f1(x))
+
+    # Area of the difference
+    num = quadgk(dif, min_alpha, max_alpha)[1]
+    # Areas of both
+    den = max(quadgk(absf1, min_alpha, max_alpha)[1],
+                  quadgk(absf2, min_alpha, max_alpha)[1])
+    push!(out, num/den)
+
+    if verbose; println("num=$num\tden=$den"); end;
+  end
+
+  return out
+end
+
+
+### INTERNAL FUNCTIONS #########################################################
+"Given a Python Polar object, it returns a julia Polar object"
+function _pyPolar2Polar(pyPolar::PyCall.PyObject, x, y)
+  polar = Polar(pyPolar[:Re], pyPolar[:alpha], pyPolar[:cl], pyPolar[:cd],
+                pyPolar[:cm], x, y)
+  return polar
+end
+### END OF airfoilprep.py CLASS ################################################
+
+
 
 end # module
---
+++ src/airfoilprep_misc.jl
@@ -0,0 +1,173 @@
+
+
+################################################################################
+# MISCELLANOUS
+################################################################################
+"Given the output of `airfoil_props()` it generates a file in aerodyn format"
+function generate_aerodyn_file(file_name, re, AoA_d,clmin,clmax,AoA_zl_d,
+                        lift_slope,cldata,cdfdata,cdpdata,cmdata; numPoints=100)
+  ncrit = "-"
+
+  # Generates splines
+  Top_Xtr = zeros(AoA_d) # Dummy arrays
+  Bot_Xtr = zeros(AoA_d)
+  (Cl_spl, Cd_spl, Cdp_spl, Cm_spl, Top_Xtr_spl, Bot_Xtr_spl, aoaZeroCl,
+  aoaMinDrag, cdmin, aoamin, aoamax) = _makeSpline(AoA_d, cldata, cdfdata, cdpdata,
+  cmdata, Top_Xtr, Bot_Xtr)
+
+  # Creates file
+  _file_name = file_name[end-3:end]==".dat" ? file_name : file_name*".dat"
+
+  write(_file_name,"    See filename for airfoil ID
+  Cl and Cd values uncorrected
+  line
+  1        Number of airfoil tables in this file
+  $(re/1e6)     Reynolds numbers in millions
+  0.0      Control setting
+  $(ncrit)     Stall angle (deg)
+  $aoaZeroCl   Zero Cn angle of attack (deg)
+  0.0   Cn slope for zero lift (dimensionless)
+  0.0   Cn extrapolated to value at positive stall angle of attack
+  0.0   Cn at stall value for negative angle of attack
+  $aoaMinDrag     Angle of attack for minimum CD (deg)
+  $cdmin   Minimum CD value\n")
+
+  f = open(_file_name,"a")
+  open(_file_name,"a") do x
+      for aoa in linspace(aoamin, aoamax, numPoints)
+          write(x,"$(aoa)\t $(Cl_spl(aoa))\t $(Cd_spl(aoa))\t $(Cm_spl(aoa)) \n")
+      end
+      write(x,"EOT")
+  end
+
+end
+
+"Plots the contour of an airfoil given in x,y"
+function plot_airfoil(x::Array{Float64,1}, y::Array{Float64,1};
+                      label="", style="-k", figfactor=1.0,
+                      title_str="Airfoil geometry")
+  # Sizes the figure
+  figsize = [7*1.5,5*0.5]*figfactor
+  xmin, xmax = -0.05, 1.05
+  yrange = (xmax-xmin)/figsize[1] * figsize[2]
+  ymin, ymax = -yrange/2, yrange/2
+  fig1 = figure("airfoil_geometry", figsize=(figsize[1], figsize[2]))
+  xlim([xmin, xmax])
+  ylim([ymin, ymax])
+
+  PyPlot.plot(x,y, style, label=label)
+  xlabel("x")
+  ylabel("y")
+  grid(true, color="0.8", linestyle="--")
+  title(title_str)
+
+  # if label!=""; legend(loc="best"); end;
+end
+
+"Receives a .dat file as pulled from airfoiltools.com containing the x and y
+contour coordinates of an airfoil, and returns arrays x and y."
+function readcontour(file_name; header_len=1)
+  x, y = Float64[], Float64[]
+
+  open(file_name) do f
+    for (i,line) in enumerate(eachline(f))
+
+      # Ignores header
+      if i<=header_len
+        nothing
+      # Parses each line
+      else
+        this_x, this_y = split(line)
+        push!(x, parse(Float64, this_x))
+        push!(y, parse(Float64, this_y))
+      end
+
+    end
+  end
+  return x,y
+end
+
+"""
+  Receives an airfoil contour and splits it up in upper and lower surfaces as
+  divided by the chord line. It returns `(upper, lower)` with `upper=(x,y)` the
+  points of the upper surface, ditto for `lower`. Both `upper` and `lower` are
+  given in increasing order in x (i.e., from leading to trailing edge).
+"""
+function splitcontour(x,y)
+  # ERROR CASES
+  if !(x[1] in [0.0, 1.0])
+    error("Invalid contour. x[1] must be either 0.0 or 1.0, got $(x[1]).")
+  end
+
+  # Flag indicating whether the contour start at the trailing or leading edge
+  start_TE = x[1]==1.0
+
+  # Find the opposite end of the contour
+  end_i = -1
+  for (i, xi) in enumerate(x)
+    if i==1
+      nothing
+    # Case of starting from the trailing edge
+    elseif start_TE && xi > x[i-1]
+      end_i = i-1
+      break
+    # Case of leading edge
+    elseif !start_TE  && xi < x[i-1]
+      end_i = i-1
+      break
+    end
+  end
+
+  # ERROR CASE
+  if end_i==-1
+    error("Logic error! End of airfoil not found!")
+  end
+
+  # Splits them up
+  x_sec1, y_sec1 = x[1:end_i], y[1:end_i]
+  x_sec2, y_sec2 = x[end_i:end], y[end_i:end]
+
+  # Sorts them from LE to TE
+  if x_sec1[1] > 0.5; reverse!(x_sec1); reverse!(y_sec1); end;
+  if x_sec2[1] > 0.5; reverse!(x_sec2); reverse!(y_sec2); end;
+
+  # Determines upper and lower surfaces
+  if mean(y_sec1) > mean(y_sec2)
+    upper = [x_sec1, y_sec1]
+    lower = [x_sec2, y_sec2]
+  else
+    upper = [x_sec2, y_sec2]
+    lower = [x_sec1, y_sec1]
+  end
+
+  return upper, lower
+end
+
+### INTERNAL FUNCTIONS #########################################################
+"Receives the output of `airfoil_props()` and returns splines of them"
+function _makeSpline(Alpha,Cl,Cd,Cdp,Cm,Top_Xtr,Bot_Xtr)
+  # Splines
+  Cl_spl = Dierckx.Spline1D(Alpha,Cl)
+  Cd_spl = Dierckx.Spline1D(Alpha,Cd+Cdp)
+  Cdp_spl = Dierckx.Spline1D(Alpha,Cdp)
+  Cm_spl = Dierckx.Spline1D(Alpha,Cm)
+  Top_Xtr_spl = Dierckx.Spline1D(Alpha,Top_Xtr)
+  Bot_Xtr_spl = Dierckx.Spline1D(Alpha,Bot_Xtr)
+
+  # Finds the zero-lift AOA
+  zerolift(aoa) = Cl_spl(aoa)
+  aoaZeroCl = Roots.fzero(zerolift, -8,10)
+
+  # Finds AOA of minimum drag
+  step = 1e-6
+  zeroMinDrag(aoa) = (Cd_spl(aoa) - Cd_spl(aoa+step))/step
+  aoaMinDrag = Roots.fzero(zeroMinDrag, -8.0,10.0)
+  cdmin = Cd_spl(aoaMinDrag)
+
+  aoamin = minimum(Alpha)
+  aoamax = maximum(Alpha)
+
+  return (Cl_spl, Cd_spl, Cdp_spl, Cm_spl, Top_Xtr_spl, Bot_Xtr_spl,
+          aoaZeroCl,aoaMinDrag, cdmin, aoamin, aoamax)
+end
+### END OF TOOLS ###############################################################
